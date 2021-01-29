0bf325786f3ce19135925dbd364d050b478baa18
Joey Yu <xiaowei.yu@dawsoncollege.qc.ca>
2021-01-29 14:02:10 -0500

    Remove player type and get ready for computer player [1/2]
    Signed-off-by: Joey Yu <xiaowei.yu@dawsoncollege.qc.ca>

--- main.go
+++ main.go
@@ -2,48 +2,70 @@ package main
 
 import "fmt"
 
+func getYOrNInputAndConvert(prompt string) bool {
+	for {
+		fmt.Print(prompt)
+		var yn string
+		fmt.Scanln(&yn)
+		if yn == "y" {
+			return true
+		} else if yn == "n" {
+			return false
+		}
+	}
+}
+
 func main() {
 	fmt.Println("Welcome to TicTacToe!")
 
 	var myBoard = initBoard()
 
-	var player1 humanPlayer
+	var player1 string
 	fmt.Print("[Player1] - Please enter your name: ")
-	player1.assignName()
+	fmt.Scanln(&player1)
 
-	var player2 humanPlayer
-	fmt.Print("[Player2] - Please enter your name: ")
-	player2.assignName()
+	fmt.Printf("Hey %v, ", player1)
+	againstComp := getYOrNInputAndConvert("wanna play against a computer? [y/n] ")
 
-	var player string
-	var marker string
+	if againstComp {
+		// TBD
+		// isSmart := getYOrNInputAndConvert("Against a smart computer? [y/n] ")
 
-	for {
-		if myBoard.isFull() {
+	} else {
+		var player2 string
+		fmt.Print("[Player2] - Please enter your name: ")
+		fmt.Scanln(&player2)
+
+		var player string
+		var marker string
+
+		for {
+			if myBoard.isFull() {
+				fmt.Print("\033[H\033[2J") // Clear console
+				fmt.Println("You two tied!")
+				myBoard.displayBoard()
+				break
+			}
+
+			if player == player1 {
+				player = player2
+				marker = "O"
+			} else {
+				player = player1
+				marker = "X"
+			}
+
 			fmt.Print("\033[H\033[2J") // Clear console
-			fmt.Println("You two tied!")
+			fmt.Printf("It's %v's turn! [%v]\n", player, marker)
 			myBoard.displayBoard()
-			break
-		}
+			myBoard.assignPos(myBoard.getPos(), marker)
 
-		if player == player1.name {
-			player = player2.name
-			marker = "O"
-		} else {
-			player = player1.name
-			marker = "X"
-		}
-
-		fmt.Print("\033[H\033[2J") // Clear console
-		fmt.Printf("It's %v's turn! [%v]\n", player, marker)
-		myBoard.displayBoard()
-		myBoard.assignPos(myBoard.getPos(), marker)
-
-		if myBoard.checkWin() {
-			fmt.Print("\033[H\033[2J") // Clear console
-			fmt.Printf("Congratulations %v, you won!\n", player)
-			myBoard.displayBoard()
-			break
+			if myBoard.checkWin() {
+				fmt.Print("\033[H\033[2J") // Clear console
+				fmt.Printf("Congratulations %v, you won!\n", player)
+				myBoard.displayBoard()
+				break
+			}
 		}
 	}
 }
